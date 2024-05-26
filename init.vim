bc396bdfd700489937275b4f7e7fe6855a709538
DraculaSlayer <138096359+DraculaSlayer@users.noreply.github.com>
2024-05-26 15:06:12 -0400

    Add files via upload

---
+++ init.vim
@@ -0,0 +1,16 @@
+set number " muestra numero de lineas
+set title " muestra el nombre del archivo
+
+set nowrap " no divine la linea
+
+set cursorline " resalta la linea
+
+" indentaciones de 2 espacios
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
+set shiftround
+set expandtab " inserta espacio debe de tab
+
+set background=dark " fondo luminoso
+"colorscheme zellner " nombre del tema
--- install.sh
+++ install.sh
@@ -1,37 +1,106 @@
 #!/bin/bash
 
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+BLUE="\e[1;34m"
+YELLOW="\e[1;33m"
+CYAN="\e[1;36m"
+NORMAL="\e[0m"
+PUNTERO="${BLUE}[${NORMAL}${YELLOW}+${NORMAL}${BLUE}]${NORMAL}"
+
+install() {
+clear
+
 termux-setup-storage
 
-echo "Inicia el programa..."
+clear
 
-echo "Instalando root-repo y x11-repo"
+echo -e "${RED}###########################################################${NORMAL}"
+echo -e "${RED}#                                                         #${NORMAL}"
+echo -e "${RED}#                                  ${NORMAL}${GREEN}Inicia el programa...${NORMAL}  ${RED}#${NORMAL}"
+echo -e "${RED}#                                                         #${NORMAL}"
+echo -e "${RED}###########################################################${NORMAL}"
 
-pkg install root-repo && pkg install x11-repo
+echo -e "${PUNTERO} ${RED}Instalando root-repo y x11-repo${NORMAL}"
 
-echo "root-repo y x11-repo Instalado :)"
+pkg install -y root-repo && pkg install -y x11-repo
 
-echo "Instalando nvim"
+echo -e "${PUNTERO} ${GREEN}root-repo y x11-repo Instalado :)${NORMAL}"
 
-pkg install neovim
+sleep 3
+clear
 
-echo "nvim Instalado :)"
+echo -e "${PUNTERO} ${RED}Instalando nvim${NORMAL}"
 
-echo "Instalando rust"
+pkg install -y neovim
+mv ~/.config/nvim/ ./nvim/init.vim
 
-pkg install rust
+echo -e "${PUNTERO} ${GREEN}nvim Instalado :)${NORMAL}"
 
-echo "rust Instalado :)"
+sleep 3
+clear
 
-echo "Instalando python"
+echo -e "${PUNTERO} ${RED}Instalando rust${NORMAL}"
 
-pkg install python
+pkg install -y rust
 
-echo "python Instalando :)"
+echo -e "${PUNTERO} ${GREEN}rust Instalado :)${NORMAL}"
 
-echo "Instalando proot-distro"
+sleep 3
+clear
 
-pkg install proot-distro
+echo -e "{${PUNTERO} ${RED}Instalando el root${NORMAL}"
 
-echo "proot-distro Instalado"
+pkg install -y proot
+git clone https://github.com/Anonymous-Zpt/T-root
+cd T-root
+bash install.sh
+./start
+cd
 
-echo "Entorno listo para hackear"
+echo -e "${PUNTERO} ${GREEN}root Instalado :)${NORMAL}"
+
+sleep 3
+clear
+
+echo -e "${PUNTERO} ${RED}Instalando terminal (myTermux)${NORMAL}"
+
+git clone --depth=1 https://github.com/mayTermux/myTermux.git
+cd ./myTermux
+export COLUMNS LINES
+./install.sh
+
+echo -e "${PUNTERO} ${GREEN}Terminal Instalada${NORMAL}"
+
+sleep 3
+clear
+
+echo -e "${BLUE}#####################################${NORMAL}"
+echo -e "${BLUE}#                                   #${NORMAL}"
+echo -e "${BLUE}#  ${NORMAL}${CYAN}Entorno listo para hackear :D:D${NORMAL}  ${BLUE}#${NORMAL}"
+echo -e "${BLUE}#                                   #${NORMAL}"
+echo -e "${BLUE}#####################################${NORMAL}"
+}
+
+clear
+echo -e "${PUNTERO} ${CYAN}Herramienta: ${NORMAL}${GREEN}Termux-tools-basic${NORMAL}"
+echo -e "${PUNTERO} ${CYAN}Creador: ${NORMAL}${GREEN}DraculsSlayer${NORMAL}"
+echo -e "${PUNTERO} ${CYAN}Github: ${NORMAL}${GREEN}https://github.com/DraculaSlayer${NORMAL}\n"
+
+echo -e "${PUNTERO} ${GREEN}Instalar: ${CYAN}1${NORMAL}${NORMAL}"
+echo -e "${PUNTERO} ${RED}Salir: ${NORMAL}${CYAN}2${NORMAL}\n"
+
+while true; do
+read -p "R: " respuesta
+if [[ $respuesta == "1" ]]
+then
+  install
+  break
+elif [[ $respuesta == "2" ]]
+then
+  echo -e "${RED}Chao :D${NORMAL}"
+  break
+else
+  echo -e "${RED}NO HAS PUESTO NADA ):(${NORMAL}"
+fi
+done
